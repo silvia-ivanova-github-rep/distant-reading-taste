e35e42531627404f6383d8c4d7dc21ff7b748c57
Cele3x <jonathan.sasse@outlook.de>
2021-02-20 12:18:24 +0100

    update RecipesSpider for chefkoch.de

--- DistantReadingTaste/DistantReadingTaste/spiders/recipes_spider.py
+++ DistantReadingTaste/DistantReadingTaste/spiders/recipes_spider.py
@@ -2,10 +2,10 @@
 
 import scrapy
 import re
-import datetime
+
+from DistantReadingTaste.items import Recipe
 
 
-# remove whitespaces, brackets and other unnecessary elements from string
 def cleanse(text):
     cleansed = re.sub(r'\(.*?\)', '', text)  # remove brackets and content
     cleansed = cleansed.strip()  # remove trailing whitespaces
@@ -21,7 +21,7 @@ class RecipesSpider(scrapy.Spider):
 
         open(self.filename, "w+").close()
         urls = [
-            'https://www.lecker.de/ofen-haehnchen-paprika-rahm-80809.html'
+            'https://www.chefkoch.de/rs/s0g31/Fruehstuecksrezepte.html'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -29,21 +29,19 @@ class RecipesSpider(scrapy.Spider):
     def parse(self, response, **kwargs):
         self.logger.info('Got successful response from {}'.format(response.url))
 
-        # get recipe title
-        title = cleanse(response.css('header.article-header h1::text').get(default=''))
-
         items = []
-        for ingredient in response.css('ul.list--ingredients li'):
-            item = {
-                'quantity': cleanse(ingredient.css('li span.quantityBlock span.ingredient-quantity::text').get(default='0')),
-                'ingredient': cleanse(ingredient.css('li span.ingredientBlock::text').get(default=''))
-            }
-            items.append(item)
 
-        with open(self.filename, 'a+') as f:
-            f.write("Results from crawling at {}\n\n".format(datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")))
-            f.write("%s\n" % title)
-            for item in items:
-                f.write("{}\n".format(','.join(str(x) for x in item.values())))
-                # f.write("{}\n".format('\t'.join(str(item[quantity]))))
-        self.log(f'Updated file {self.filename}')
+        # get infos from overview page
+        for overviewRecipe in response.css('article.rsel-item'):
+            title = overviewRecipe.css('h2.ds-heading-link::text').get(default='')
+            country = 'DE'
+            url = overviewRecipe.css('a.rsel-recipe::attr("href")').get(default='')  # maybe ::first
+            recipe = Recipe(title=title, country=country, url=url)
+
+            items.append(recipe)
+            self.logger.info('Added item {}'.format(recipe.items()))
+
+        # next_page = response.css('ul.ds-pagination li.ds-next a::attr("href")').get()
+        # if next_page is not None:
+        #     self.logger.info('Proceeding with next page')
+        #     yield response.follow(next_page, self.parse)
